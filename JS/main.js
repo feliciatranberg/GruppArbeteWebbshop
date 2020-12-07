22f7e988451ec99357551e917cf070b02b4d272e
feliciatranberg <felicia@substansgbg.se>
2020-12-07 15:10:15 +0100

    mainPage

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,3 @@
 $(function() {
 
 });
-
-function navbar () {
-    let x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
