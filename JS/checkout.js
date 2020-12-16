7cd76638a8c3d05878ae30c76f629108546ff558
feliciatranberg <felicia@substansgbg.se>
2020-12-16 13:53:56 +0100

    some updates on checkoutConfirm

--- JS/checkout.js
+++ JS/checkout.js
@@ -2,61 +2,23 @@ $(function() {
 
   $("button").on('click', function(){
     dialog.dialog('open');
+    orderComfirm();
   });
 
   let dialog = $("#dialog").dialog({
     autoOpen: false,
     modal: true,
-   buttons: {
-     Ok: function() {
-       $(this).dialog('close');   
-  }}
+    width: 'auto', // overcomes width:'auto' and maxWidth bug
+    maxWidth: 600,
+    height: 'auto',
+    fluid: true, //new option
+    resizable: false
 });
-    
- 
-//Felicia modalfunktion
-    // $.fn.serializeObject = function() {
-    //     let $form = this.is("form") ? this : this.find("form"),
-    //       obj = {};
-        
-    //     $.each($form.serializeArray(), function() {
-          
-    //       if (this.name in obj) {
-    //         // Should be array
-    //         if (!$.isArray(obj[this.name])) {
-    //           obj[this.name] = [obj[this.name]];
-    //         }
-    //         // Push value
-    //         obj[this.name].push(this.value);
-    //       } else {
-    //         // Set value
-    //         obj[this.name] = this.value;
-    //       }
-    //     });
-    //     return obj;
-    //   };
-      
-    //   $("form").on('submit', function(e){
-    //     dialog.dialog('open');
-    //     let $form = $(this);
-    //     e.preventDefault();
-    //    (JSON.stringify($form.serializeObject()));
-    //     console.log($form.serializeObject());
-    //     // alert(JSON.stringify($form.serializeObject()));
-      
-   
-    //   $("#dialog-content")
-    //   $("<p>")
-    //   .html(JSON.stringify($form.serializeObject()))
-    //   .appendTo("#dialog-content");
-    // });  
 
           
     finalCheckoutList();
       });
 
-
-  
   function finalCheckoutList(){
 
     let checkoutArrFromLS = JSON.parse(localStorage.checkoutArr);
@@ -107,6 +69,44 @@ $(function() {
     });
 
   };
+
+
+  function orderComfirm() {
+
+    let checkoutArrFromLS = JSON.parse(localStorage.checkoutArr);
+    let checkoutProductsDiv = ($("#dialog"));
+
+      $.each(checkoutArrFromLS,(i,item)=>{
+    let productDiv = ($("<div>"))
+            .addClass("product-Div")
+            .appendTo(checkoutProductsDiv);
+
+            ($("<img>"))
+            .attr("src",item.image)
+            .addClass("productImage")
+            .appendTo($(productDiv));
+
+    let productTextDiv = ($("<div>"))
+            .addClass("product-text-div")
+            .appendTo($(productDiv));
+
+            ($("<h5>"))
+            .html(item.name)
+            .appendTo($(productTextDiv));
+            
+            ($("<h1>"))
+            .html(item.price + ":-")
+            .appendTo($(productTextDiv));
+
+            ($("<span>"))
+            .html(item.size + " ")
+            .appendTo($(productTextDiv));
+  
+              });
+  };
+  
+
+
 
 
 
