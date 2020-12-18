7b0242d0073b2aeee8dcd7eea02f618d73fb7bf4
max-da <max.dahlbom@outlook.com>
2020-12-18 14:22:35 +0100

    js initial refactoring

--- JS/product-page-t-shirt.js
+++ JS/product-page-t-shirt.js
@@ -23,43 +23,40 @@ let shirt5 = new shopItem("T-shirt","2:2","Subtle Tie Dye","Rosa","Small",300, "
 let shirt6 = new shopItem("T-shirt","2:2","Chakra","Vit/Svart","Small",300, "/images/t-shirts/t-shirt-6.jpg","Chakra T-shirt",true,0,false)
 
 let tshirtProducter = [shirt1,shirt2,shirt3,shirt4,shirt5,shirt6]
-let checkoutArr = [];
+//let checkoutArr = [];
 //let priceArray = [];
 let totalCount = false;
-//localStorage.clear();
-$(function() {
-    let wrapperProductPage = ($("<div>"))
-        .addClass("wrapperProductPage")
-        .appendTo($("body"));
- //CARTEMPTYMESSAGE
- ($("<div>"))
- .attr("id","cartEmptyMessage")
- .addClass("hidden")
- .appendTo($("#shoppingcartExtend"));  
- ($("<em>"))
- .html("Det verkar som att din varukorg är tom")
- .appendTo($("#cartEmptyMessage"));
-    $.each(tshirtProducter,(i,item)=>{
 
-    
-          
-     
-                let itemDiv = ($("<div>"))
+
+$(function() {
+      let wrapperProductPage = ($("<div>"))
+            .addClass("wrapperProductPage")
+            .appendTo($("body"));
+ //Meddelande som visas när varukorg är tom
+      $("<div>")
+            .attr("id", "cartEmptyMessage")
+            .addClass("hidden")
+            .appendTo($("#shoppingcartExtend"));
+       $("<em>")
+            .html("Det verkar som att din varukorg är tom...")
+            .appendTo($("#cartEmptyMessage"));
+
+//Loop för att rendera produkter på skärm. När man klickar på en produkt sparas produkt i ls, och man skickas till till htmlsida för 
+//specifik produkt
+    $.each(tshirtProducter,(i,item)=>{
+        let itemDiv = ($("<div>"))
                 .addClass("itemDiv")
-                .on("click",{item:item},(e)=>{
+                .on("click",()=>{
+
                     window.location.href = "product-specific.html";
-                    let itemObject = e.data.item;
-                   // checkoutArr.push(itemObject)
-                    localStorage.setItem("itemObject",JSON.stringify(itemObject));
-                    console.log(checkoutArr)
-                    //shoppingCartDiv.removeClass("hidden");
-                    //localStorage.setItem("checkoutArr",JSON.stringify(checkoutArr));
+        
+                    localStorage.setItem("itemObject",JSON.stringify(item));
+                
                     ($("#shoppingCart")).empty();
                     checkoutCreator();
                     totalCreator(item);
                     
                 })
-               
                 .appendTo($(wrapperProductPage));
                 
 
@@ -82,192 +79,147 @@ $(function() {
              })
 
 
-            //Div för shoppingcart or elaterad saker
+//För checkout
             let shoppingCartDiv = $("<div>")
-            .addClass("hidden")
-            .attr("id","shoppingCartDiv")
-          
-            .appendTo($("#shoppingcartExtend"));
+                  .addClass("hidden")
+                  .attr("id","shoppingCartDiv")
+                  .appendTo($("#shoppingcartExtend"));
 
             ($("<div>"))
-            .attr("id","shoppingCart")
-            .appendTo(shoppingCartDiv);
+                   .attr("id","shoppingCart")
+                   .appendTo(shoppingCartDiv);
 
             ($("<button>"))
-            .attr("id","goToCheckout")
-            .html("Go to checkout")
-            .on("click",()=>{
-              window.location.href = "checkout.html";
-            })
-            .appendTo(shoppingCartDiv);
+                  .attr("id","goToCheckout")
+                  .html("Go to checkout")
+                  .on("click",()=>{
+                       window.location.href = "checkout.html";
+                   })
+                 .appendTo(shoppingCartDiv);
 
-
-            $("#shoppingcartButton")
-   
-            .on("click",()=>{
+//Varukorg i nav-bar. Bestämmer om den ska visa varukorg eller meddelande för varukorg
+  //samt om den ska visa varukorg eller gå till checkout-sida beroende på mobil eller desktop
+                 $("#shoppingcartButton")
+                 .on("click", () => {
+           
+                 if (window.matchMedia("(max-width: 767px)").matches) {
+           
+                           if (JSON.parse(localStorage.checkoutArr).length === 0) {
+                                 $("#cartEmptyMessage").toggleClass("hidden");
+                            } 
+                           else {
+                                 window.location.href = "/HTML/checkout.html";
+                            }
+                   } 
+                 else {
+                          shoppingCartDiv.toggleClass("hidden");
+                          cartCounterDisplay();
+                          $("#shoppingCart").empty();
+                          checkoutCreator();
+                          totalCreator();
                 
-              if (window.matchMedia('(max-width: 767px)').matches) {
-                if (JSON.parse(localStorage.checkoutArr).length === 0){
-                  ($("#cartEmptyMessage"))
-                   .toggleClass("hidden")
-                }
-                else {
-                window.location.href = "checkout.html";
-                }
-              }
-              else {
-             
-                shoppingCartDiv.toggleClass("hidden");
+                        if (JSON.parse(localStorage.checkoutArr).length === 0) {
+           
+                            $("#cartEmptyMessage").toggleClass("hidden");
+                 }
+               }
+             });
          
-             cartCounterDisplay();
-               
-             ($("#shoppingCart")).empty();
-             checkoutCreator();
-             totalCreator(); 
-             if (JSON.parse(localStorage.checkoutArr).length === 0) {
-              ($("#cartEmptyMessage"))
-              .toggleClass("hidden")
-              }
-             }
-            });
-            //($("#cartNumberDisplay")).removeClass("hidden").html(JSON.parse(localStorage.checkoutArr).length);
             cartCounterDisplay();
               //onload           
     });
 
+//Funktion för varukorg. Hämtar värden från ls. 
+function checkoutCreator(i, item) {
+  
+  let checkout = $("#shoppingCart");
+  let checkoutArrFromLS = JSON.parse(localStorage.checkoutArr);
+  let priceArrayFromLS = JSON.parse(localStorage.priceArray);
 
-    function checkoutCreator(i,item){
-      console.log("checkoutCreator körs")
-      let checkout = ($("#shoppingCart"))
-          let checkoutArrFromLS = JSON.parse(localStorage.checkoutArr);
-        console.log(checkoutArrFromLS);
-          
-          $.each(checkoutArrFromLS,(i,item)=>{
+      $.each(checkoutArrFromLS, (i, item) => {
 
-             let shoppingCartItemDiv=($("<div>"))
-                  .addClass("shoppingCartItemDiv")
-                  .appendTo($(checkout));
+           let shoppingCartItemDiv = $("<div>")
+              .addClass("shoppingCartItemDiv")
+              .appendTo($(checkout));
 
-              let shoppingCartImgDiv=($("<div>"))
-                  .addClass("shoppingCartImgDiv")
-                  .appendTo($(checkout));
+           let shoppingCartImgDiv = $("<div>")
+              .addClass("shoppingCartImgDiv")
+              .appendTo($(checkout));
 
-                  ($("<img>"))
-                  .attr("src",item.image)
-                  .appendTo($(shoppingCartImgDiv));
-                  
-                  ($("<h5>"))
-                      .html(item.name)
-                      .appendTo($(shoppingCartItemDiv));
-                  ($("<button>"))
-                    .addClass("shoppingCartDeleteBtn")
-                     .addClass("fas fa-times")
-                      .appendTo($(shoppingCartItemDiv))
-                      .on("click",()=>{
-                          checkoutArrFromLS.splice(i,1);
-                         
-                          //checkoutArr.splice(i,1);
-                        
-                          localStorage.setItem("checkoutArr",JSON.stringify(checkoutArrFromLS));
-                          cartCounterDisplay();
-                          let priceArrayFromLS =JSON.parse(localStorage.priceArray);
-                          priceArrayFromLS.splice(i,1);
-                          localStorage.setItem("priceArray",JSON.stringify(priceArrayFromLS));
-                          /*if (priceArrayFromLS.length == 0){
-                            totalCount = false;
-                            ($("#checkoutTotal")).html("");
-                          }*/
-                          checkout.empty();
-                          //($("#checkoutTotal")).empty();
-                          checkoutCreator();
-                          
-                          totalCreator();
-                          
-                          
-    
-                               
-                              
-                      })
-                          
-                         
-                      
-          });
+              $("<img>").attr("src", item.image).appendTo($(shoppingCartImgDiv));
+
+              $("<h5>").html(item.name).appendTo($(shoppingCartItemDiv));
+
+           $("<button>")
+
+              .addClass("shoppingCartDeleteBtn")
+              .addClass("fas fa-times")
+              .appendTo($(shoppingCartItemDiv))
+              .on("click", () => {
+
+                  checkoutArrFromLS.splice(i, 1);
+                  localStorage.setItem("checkoutArr", JSON.stringify(checkoutArrFromLS));
+
+                  cartCounterDisplay();
         
-    
-    }
-//skapar första gången ptag för prissumma, adderar/subtrarherar följande ggr från array o renderar
-function totalCreator(specificProduct){
-    console.log("jag klrs")
-    let bitch = ($("#checkoutTotal"));
-   
-    if (totalCount == false) {
-      console.log("Totalcount är false")
+                  priceArrayFromLS.splice(i, 1);
+                  localStorage.setItem("priceArray", JSON.stringify(priceArrayFromLS));
   
-       console.log("hello")
-       
-  
-      let priceArrayFromLS = JSON.parse(localStorage.priceArray)
-    
-   
-    let arrSum = priceArrayFromLS.reduce((a, b) => a + b, 0);
-    console.log(arrSum);
-    if (arrSum==0){
-        arrSum = "";
-    }
-    ($("<p>"))
-    .attr("id","checkoutTotal")
-    .html(arrSum)
-    .appendTo($("#shoppingCartDiv"));
-    totalCount = true;
-  }
-  /*else
-    {
-       if (priceArray == 0){
-            bitch.addClass("hidden");
-       }
-       else {
-           bitch.removeClass("hidden");
-       }
-        if (specificProduct != undefined) {
-          let priceArray =JSON.parse(localStorage.priceArray);
-        priceArray.push(specificProduct.price);
-        localStorage.setItem("priceArray",JSON.stringify(priceArray));
-        }
-        let arrSum = priceArray.reduce((a, b) => a + b, 0);
-        console.log(priceArray);
-       
-        ($("#checkoutTotal"))
-        .html(arrSum)
-        .appendTo($("#shoppingCartDiv"));
-        
-    }*/
-  
-    else {
-      let priceArrayFromLS = JSON.parse(localStorage.priceArray)
-      console.log("Totalcount e true")
-      localStorage.setItem("priceArray",JSON.stringify(priceArrayFromLS));
-      //console.log(priceArray);
-    let arrSum = priceArrayFromLS.reduce((a, b) => a + b, 0);
-    console.log(arrSum);
-    ($("#checkoutTotal")).empty();
-    ($("#checkoutTotal"))
-    .html(arrSum)
-    .appendTo($("#shoppingCartDiv"));
-    if (priceArrayFromLS.length == 0 ){
-      totalCount = false;
-      ($("#checkoutTotal")).remove();
-      console.log("HELT SLUT")
-    }
-    }
-  }
+                   checkout.empty();
+                   checkoutCreator();
+                   totalCreator();
+      });
+  });
+}
 
-  function cartCounterDisplay(){
-    let checkoutArrFromLS = JSON.parse(localStorage.checkoutArr);
-    ($("#cartNumberDisplay")).removeClass("hidden").html(checkoutArrFromLS.length);
-    if (checkoutArrFromLS.length ===0){
-     ($("#cartNumberDisplay")).addClass("hidden");
-     ($(shoppingCartDiv)).addClass("hidden");
-     console.log("korgen en tom jao")
-    }
+//Skapar ptag för prissumma om den inte redan existerar, då ändrar den bara värdet. Tar bort
+//Om summan inte finns
+function totalCreator(specificProduct) {
+ 
+
+  if (totalCount == false) {
+      let priceArrayFromLS = JSON.parse(localStorage.priceArray);
+      
+      let arrSum = priceArrayFromLS.reduce((a, b) => a + b, 0);
+      
+      if (arrSum == 0) {
+          arrSum = "";
+      }
+
+      $("<p>")
+          .attr("id", "checkoutTotal")
+          .html(arrSum)
+          .appendTo($("#shoppingCartDiv"));
+          totalCount = true;
+  } 
+  else {
+      let priceArrayFromLS = JSON.parse(localStorage.priceArray);
+       localStorage.setItem("priceArray", JSON.stringify(priceArrayFromLS));
+
+      let arrSum = priceArrayFromLS.reduce((a, b) => a + b, 0);
     
+       $("#checkoutTotal").empty();
+       $("#checkoutTotal").html(arrSum).appendTo($("#shoppingCartDiv"));
+
+      if (priceArrayFromLS.length == 0) {
+
+           totalCount = false;
+          $("#checkoutTotal").remove();
+  
+    }
   }
+}
+
+//Visar antal objekt i varukorgen.
+function cartCounterDisplay() {
+
+  let checkoutArrFromLS = JSON.parse(localStorage.checkoutArr);
+   $("#cartNumberDisplay").removeClass("hidden").html(checkoutArrFromLS.length);
+
+  if (checkoutArrFromLS.length === 0) {
+
+    $("#cartNumberDisplay").addClass("hidden");
+    $(shoppingCartDiv).addClass("hidden");
+  
+  }
+}
